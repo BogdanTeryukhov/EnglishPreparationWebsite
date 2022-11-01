f25058a4c83c9968f7ba9d18a780b7cd8c388940
BogdanTeryukhov <andrejteryukhov5@gmail.com>
2022-11-01 16:52:15 +0300

    added static repository for js, css, images and MVC Config which add these resources into project

---
+++ src/main/resources/static/js/testForTravelAndTransport.js
@@ -0,0 +1,16 @@
+let button = document.querySelector('#button')
+button.addEventListener('click', function () {
+    let inputs = document.querySelectorAll('#test input')
+    for (let input of inputs) {
+        if (input.value === input.dataset.right){
+            input.classList.add('correct')
+        }else{
+            input.classList.add('incorrect')
+        }
+    }
+})
+
+let resetButton = document.querySelector('#reset-button')
+resetButton.addEventListener('click',function (){
+    window.location.reload()
+})
