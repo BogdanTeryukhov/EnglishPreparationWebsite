d83efa80751d557fa1f5b9bc39b00f2c14d3cbed
BogdanTeryukhov <andrejteryukhov5@gmail.com>
2023-03-19 12:38:16 +0300

    added request mapping for AdminsUsersEditorController

--- src/main/java/blogapp/controllers/AdminsUsersEditorController.java
+++ src/main/java/blogapp/controllers/AdminsUsersEditorController.java
@@ -5,18 +5,16 @@ import blogapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/admin/users")
 public class AdminsUsersEditorController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping
     public String users(Model model){
         model.addAttribute("title", "Страница пользователей");
         model.addAttribute("users", userService.getAllUsers());
@@ -24,20 +22,20 @@ public class AdminsUsersEditorController {
         return "users";
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public String deleteUser(@PathVariable Long id){
         userService.deleteUserById(id);
-        return "redirect:/users";
+        return "redirect:/admin/users";
     }
 
-    @GetMapping("/users/edit/{id}")
+    @GetMapping("/edit/{id}")
     public String editStudent(@PathVariable Long id, Model model){
         model.addAttribute("user", userService.getUserById(id));
         return "editUser";
     }
 
 
-    @PostMapping("/users/{id}")
+    @PostMapping("/{id}")
     public String updateUser(@PathVariable Long id,
                              @ModelAttribute("user") User user,
                              Model model){
@@ -49,6 +47,6 @@ public class AdminsUsersEditorController {
         existingUser.setPassword(user.getPassword());
 
         userService.updateUser(existingUser);
-        return "redirect:/users";
+        return "redirect:/admin/users";
     }
 }
